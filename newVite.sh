d052ba88558b156eda13f031ed4a962481bc79a6
Evan Wills <evan.wills@acu.edu.au>
2022-02-03 16:55:39 +1100

    add user input validation to newvite

--- newVite.sh
+++ newVite.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
 
-element="$1";
-
 echo;
 echo;
-echo 'About to create a new "'$element'" web component';
-echo 'using Lit-Element & TypeScript';
+
+if [ ! -z "$1" ]
+then	element=$(echo "$1" | grep '^[a-z]\+\(-[a-z]\+\)\+$');
+
+	if [ ! -z "$element" ]
+	then	echo 'About to create a new <'$element'></'$element'> web component';
+		echo 'using Lit-Element & TypeScript';
+		echo;
+		echo 'First Vite will do all the scaffolding for your component.';
+		echo "Next we'll install all the node modules.";
+		echo "Then we'll launch a development server for your new component.";
+		echo "And finally, we'll launch VS Code so you can start work.";
+
+		npm init vite@latest $element -- --template lit-ts;
+
+		cd $element;
+
+		npm install;
+		npm run dev &
+		code -n $(pwd) &
+	else
+		echo 'The name you specified ("'$1'") is not valid';
+	fi
+else
+	echo 'You must specify a name for your element';
+fi
 echo;
-
-npm init vite@latest $element -- --template lit-ts
-
-cd $element;
-
-npm install;
-npm run dev;
-vscode;
